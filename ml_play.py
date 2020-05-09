5617ebc455c0583a225c7ecba14914d00d65a5c1
qscesz84563 <qscesz84563@gmail.com>
2020-05-09 17:35:54 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -33,7 +33,7 @@ def ml_loop(side: str):
     # === Here is the execution order of the loop === #
     # 1. Put the initialization code here
     ball_served = False
-    filename = path.join(path.dirname(__file__), 'save', 'trained_midel.pickle')
+    filename = path.join(path.dirname(__file__), 'save', 'trained_model.pickle')
     with open(filename, 'rb') as file:
         tree = pickle.load(file)
 
